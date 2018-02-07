4d97da30f235e2d4c5ed242c316670bd503bdef7
ratkus91 <sementzovandrey@gmail.com>
2018-02-07 11:55:36 +0300

    Create index.d.ts
    Added typings

---
+++ index.d.ts
@@ -0,0 +1,34 @@
+import * as React               from 'react';
+
+import { StyleProp, ViewStyle } from 'react-native';
+
+interface PickerProps {
+    style?: StyleProp<ViewStyle>,
+    onValueChange: Function,
+    pickerData?: any[],
+    selectedValue: any,
+    textColor?: string,
+    textSize?: number,
+    itemStyle?: StyleProp<ViewStyle>,
+    itemSpace?: number,
+    selectedIndex?: number
+}
+
+interface DatePickerProps {
+    labelUnit?: {
+        year: string,
+        month: string,
+        day: string
+    },
+    date?: Date,
+    maximumDate?: Date,
+    minimumDate?: Date,
+    mode?: 'date' | 'time' | 'datetime',
+    onDateChange: Function
+}
+
+class Picker extends React.Component<PickerProps, any> {}
+
+class DatePicker extends React.Component<DatePickerProps, any> {}
+
+export { Picker, DatePicker };
